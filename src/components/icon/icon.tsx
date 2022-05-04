af89b0273f9ba31d3be9bbe28d7e18a5ac7283b9
虎正玺 <15264316179@163.com>
2022-05-04 23:17:52 +0800

    feat: icon added

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,25 +1,24 @@
 /**
- * Created by Tiger on 04/05/2022.
+ * Created by jason on 2022/5/3.
  */
-import {FontAwesomeIcon, FontAwesomeIconProps} from "@fortawesome/react-fontawesome";
-import React, {PropsWithChildren} from "react";
-import classNames from "classnames";
+import {FontAwesomeIconProps, FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {PropsWithChildren} from 'react';
+import classNames from 'classnames';
 
-export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark';
+export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
 
 export interface IconProps extends FontAwesomeIconProps {
-  theme?: ThemeProps
+  theme?: ThemeProps;
 }
 
 const Icon: React.FC<PropsWithChildren<IconProps>> = (props) => {
-  const {theme, className, ...restProps} = props
+  const {className, theme, ...restProps} = props;
   const classes = classNames('jason-icon', className, {
     [`icon-${theme}`]: theme
-  })
-
+  });
   return (
-    <FontAwesomeIcon className={classes} {...restProps}/>
-  )
-}
+    <FontAwesomeIcon className={classes} {...restProps} />
+  );
+};
 
-export default Icon
+export default Icon;
