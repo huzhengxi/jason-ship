d754617f4a294a42f95d7d7bc823616cedceec39
Tiger <15264316179@163.com>
2022-05-11 00:52:27 +0800

    refactor: Button enum 改为 type 联合类型

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
 import React from 'react';
-import Menu from './components/menu';
-import MenuItem from './components/menu/menuItem';
-import SubMenu from "./components/menu/subMenu";
 
 function App() {
   return (
     <div className="App">
-      <Menu
-        defaultIndex={'0'}
-        mode={'horizontal'}
-        onSelect={(selectedIndex)=> alert(`${selectedIndex}`) }
-        defaultOpenSubmenus={['2 ']}
-      >
-        <MenuItem>
-          cool link
-        </MenuItem>
-        <MenuItem >
-          cool link1
-        </MenuItem>
-        <SubMenu title={'dropdown'}>
-          <MenuItem>
-            dropdown1
-          </MenuItem>
-          <MenuItem>
-            dropdown2
-          </MenuItem>
-        </SubMenu>
-        <MenuItem>
-          cool link2
-        </MenuItem>
-      </Menu>
     </div>
   );
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,17 +5,10 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export enum ButtonSize {
-  Large = 'lg',
-  Small = 'sm'
-}
+export type ButtonSize = 'lg' | 'sm'
 
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link'
-}
+export type ButtonType = 'primary' | 'danger' | 'link' | 'default'
+
 
 interface BaseButtonProps {
   className?: string;
@@ -44,9 +37,9 @@ const Button: React.FC<ButtonProps> = (props) => {
     {
       [`btn-${btnType}`]: btnType,
       [`btn-${size}`]: size,
-      'disabled': (btnType === ButtonType.Link) && disabled
+      'disabled': (btnType === 'link') && disabled
     });
-  if (btnType === ButtonType.Link) {
+  if (btnType === 'link') {
     return (
       <a
         className={classes}
@@ -70,7 +63,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default
+  btnType: 'default'
 };
 
 
